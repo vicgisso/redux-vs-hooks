943169a3adbfa7f7cd5bdcde73896f1ec0bfc9e0
Vicent Gisbert <vicent.gisbert@flywire.com>
2022-09-08 14:22:10 +0200

    using redux hooks useDispatch and useSelector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import * as actions from './redux/actions';
 
 const Display = ({ count }) => <h1>Valor del contador: {count}</h1>;
@@ -11,22 +11,16 @@ const Decrement = ({ decrement }) => (
   <button onClick={() => decrement()}>Restar 1</button>
 ); 
 
-const App = ({ count, increment, decrement }) => {  
+const App = () => {
+  const count = useSelector(store => store.count);
+  const dispatch = useDispatch();
   return (    
     <>         
       <Display count={count} />
-      <Increment increment={increment} />
-      <Decrement decrement={decrement} />
+      <Increment increment={() => dispatch(actions.increment())} />
+      <Decrement decrement={() => dispatch(actions.decrement())} />
     </>  
   );
 }; 
 
-export default connect(
-  store => ({
-    count: store.count
-  }),
-  {
-    increment: actions.increment, 
-    decrement: actions.decrement
-  }
-)(App);
+export default App;
