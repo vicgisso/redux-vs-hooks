76e8f1251547fc6bddbef1af0580b92996767721
Vicent Gisbert <vicent.gisbert@flywire.com>
2022-09-08 13:32:36 +0200

    context and reducer hooks implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React from 'react';
+import React, { createContext, useContext, useReducer } from "react";
 
-const Display = ({ counter }) => <h1>Valor del contador: {counter}</h1>;
+const CounterContext = createContext();
 
-const Increment = ({ addToCounter }) => (  
-  <button onClick={() => addToCounter(1)}>Sumar 1</button>
-); 
-const Decrement = ({ addToCounter }) => (  
-  <button onClick={() => addToCounter(-1)}>Restar 1</button>
-); 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_COUNTER": {
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+    }
+    default:
+      return state;
+  }
+};
 
-const App = () => {  
-  const [counter, setCounter] = React.useState(0);  
-  const addToCounter = value => setCounter(counter + value);  
-  return (    
-    <>      
-      <h1>useReducer</h1>      
-      <Display counter={counter} />      
-      <Increment addToCounter={addToCounter} />      
-      <Decrement addToCounter={addToCounter} />    
-    </>  
+const initialState = {
+  counter: 0
+};
+
+const CounterContextProvider = props => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <CounterContext.Provider value={{ state, dispatch }}>
+      {props.children}
+    </CounterContext.Provider>
   );
-}; 
+};
+
+const Display = () => {
+  const { state } = useContext(CounterContext);
+  return <h1>Valor del contador: {state.counter}</h1>;
+};
+
+const Increment = () => {
+  const { dispatch } = useContext(CounterContext);
+  return (
+    <button
+      onClick={() =>
+        dispatch({
+          type: "ADD_TO_COUNTER",
+          value: 1
+        })
+      }
+    >
+      Sumar 1
+    </button>
+  );
+};
+const Decrement = () => {
+  const { dispatch } = useContext(CounterContext);
+  return (
+    <button
+      onClick={() =>
+        dispatch({
+          type: "ADD_TO_COUNTER",
+          value: -1
+        })
+      }
+    >
+      Restar 1
+    </button>
+  );
+};
+const App = () => {
+  return (
+    <CounterContextProvider>
+      <Display />
+      <Increment />
+      <Decrement />
+    </CounterContextProvider>
+  );
+};
 
 export default App;
