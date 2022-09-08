3f91f029052ddeaaee40996f1aef13024534d3a7
Vicent Gisbert <vicent.gisbert@flywire.com>
2022-09-08 14:17:07 +0200

    same code style

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from './redux/actions';
 
-// const Display = ({ counter }) => <h1>Valor del contador: {counter}</h1>;
+const Display = ({ count }) => <h1>Valor del contador: {count}</h1>;
 
-// const Increment = ({ increment }) => (  
-//   <button onClick={() => increment()}>Sumar 1</button>
-// ); 
-// const Decrement = ({ decrement }) => (  
-//   <button onClick={() => decrement()}>Restar 1</button>
-// ); 
+const Increment = ({ increment }) => (  
+  <button onClick={() => increment()}>Sumar 1</button>
+); 
+const Decrement = ({ decrement }) => (  
+  <button onClick={() => decrement()}>Restar 1</button>
+); 
 
 const App = ({ count, increment, decrement }) => {  
   return (    
     <>         
-      <h1>Valor del contador: {count}</h1>     
-      <button onClick={() => {console.log('increment');increment();}}>Sumar 1</button>     
-      <button onClick={() => {console.log('decrement');decrement();}}>Restar 1</button> 
+      <Display count={count} />
+      <Increment increment={increment} />
+      <Decrement decrement={decrement} />
     </>  
   );
 }; 
